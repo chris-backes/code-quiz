ff6c24a06a463a9aae0f1975d70ce03d631cc8ff
Christopher Backes <you@example.com>
2021-12-11 14:05:11 -0500

    fixed the event listener bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,136 +7,110 @@ let timeLeft = 60;
 var rightAnswer = function () {
   document.querySelector(".right-wrong").innerHTML = "Correct!";
 };
+
 var wrongAnswer = function () {
   document.querySelector(".right-wrong").innerHTML = "Wrong!";
-  timeLeft = timeLeft - 15;
+  timeLeft = timeLeft - 10;
 };
+
+function eventsAndButtons(answerA, answerB, answerC) {
+  responseContentOne.textContent = answerA;
+  responseContentTwo.textContent = answerB;
+  responseContentThree.textContent = answerC;
+  document.querySelector("#prompt").appendChild(responseContentOne);
+  document.querySelector("#prompt").appendChild(responseContentTwo);
+  document.querySelector("#prompt").appendChild(responseContentThree);
+  responseContentOne.setAttribute("class", "btn a");
+  responseContentTwo.setAttribute("class", "btn b ");
+  responseContentThree.setAttribute("class", "btn c");
+}
+
+function removeButtons() {
+  responseContentOne.remove();
+  responseContentTwo.remove();
+  responseContentThree.remove();
+}
 
 var questionOne = function () {
   document.querySelector("#question-title").innerHTML =
     "What is the correct way to connect an external js file to an HTML file";
-  responseContentOne.textContent = "<js>";
-  responseContentTwo.textContent = "<script>";
-  responseContentThree.textContent = "<javascript>";
-  document.querySelector("#prompt").appendChild(responseContentOne);
-  document.querySelector("#prompt").appendChild(responseContentTwo);
-  document.querySelector("#prompt").appendChild(responseContentThree);
-  responseContentOne.setAttribute("class", "btn");
-  responseContentTwo.setAttribute("class", "btn");
-  responseContentThree.setAttribute("class", "btn");
-  responseContentTwo.setAttribute("id", "b");
-  responseContentOne.setAttribute("id", "a");
-  responseContentThree.setAttribute("id", "c");
 
-  document.querySelector("#b").addEventListener("click", function () {
-    rightAnswer();
-    questionTwo();
-  });
-  document.querySelector("#a").addEventListener("click", function () {
-    wrongAnswer();
-    questionTwo();
-  });
-  document.querySelector("#c").addEventListener("click", function () {
-    wrongAnswer();
-    questionTwo();
-  });
+  eventsAndButtons("<js>", "<script>", "<javascript>");
+
+  document.querySelector(".a").addEventListener("click", questionTwo);
+  document.querySelector(".b").addEventListener("click", questionTwo);
+  document.querySelector(".c").addEventListener("click", questionTwo);
+  document.querySelector(".a").addEventListener("click", wrongAnswer);
+  document.querySelector(".b").addEventListener("click", rightAnswer);
+  document.querySelector(".c").addEventListener("click", wrongAnswer);
 };
 
 var questionTwo = function () {
   document.querySelector("#question-title").innerHTML =
     "What is the correct way to connect an external js file to an HTML file";
-  responseContentOne.textContent = "<script src='....js'>";
-  responseContentTwo.textContent = "<script href='....js'>";
-  responseContentThree.textContent = "<script name='....js'>";
-  //This does not fix the problem specified below
-  document.querySelector("#b").removeEventListener("click", function () {
-    rightAnswer();
-    questionTwo();
-  });
-  document.querySelector("#a").removeEventListener("click", function () {
-    wrongAnswer();
-    questionTwo();
-  });
-  document.querySelector("#c").removeEventListener("click", function () {
-    wrongAnswer();
-    questionTwo();
-  });
 
-  document.querySelector("#a").addEventListener("click", function () {
-    rightAnswer();
-    questionThree();
-  });
-  document.querySelector("#b").addEventListener("click", function () {
-    wrongAnswer();
-    questionThree();
-  });
-  document.querySelector("#c").addEventListener("click", function () {
-    wrongAnswer();
-    questionThree();
-  });
+  removeButtons();
+  eventsAndButtons(
+    "<script src='....js'>",
+    "<script href='....js'>",
+    "<script name='....js'>"
+  );
+
+  document.querySelector(".a").addEventListener("click", questionThree);
+  document.querySelector(".b").addEventListener("click", questionThree);
+  document.querySelector(".c").addEventListener("click", questionThree);
+  document.querySelector(".a").addEventListener("click", rightAnswer);
+  document.querySelector(".b").addEventListener("click", wrongAnswer);
+  document.querySelector(".c").addEventListener("click", wrongAnswer);
 };
-//Bug: timer disappears right here, it is because the event listeners stay active after being pressed, and reactivate when another button is pressed
+
 var questionThree = function () {
   document.querySelector("#question-title").innerHTML =
     "How do you write 'Hello World' in an alert box?";
-  responseContentOne.textContent = "prompt('Hello World')";
-  responseContentTwo.textContent = "confirm('Hello World')";
-  responseContentThree.textContent = "alert('Hello World')";
 
-  document.querySelector("#c").addEventListener("click", function () {
-    rightAnswer();
-    questionFour();
-  });
-  document.querySelector("#a").addEventListener("click", function () {
-    wrongAnswer();
-    questionFour();
-  });
-  document.querySelector("#b").addEventListener("click", function () {
-    wrongAnswer();
-    questionFour();
-  });
+  removeButtons();
+  eventsAndButtons(
+    "prompt('Hello World')",
+    "confirm('Hello World')",
+    "alert('Hello World')"
+  );
+
+  document.querySelector(".a").addEventListener("click", questionFour);
+  document.querySelector(".b").addEventListener("click", questionFour);
+  document.querySelector(".c").addEventListener("click", questionFour);
+  document.querySelector(".a").addEventListener("click", wrongAnswer);
+  document.querySelector(".b").addEventListener("click", wrongAnswer);
+  document.querySelector(".c").addEventListener("click", rightAnswer);
 };
 
 var questionFour = function () {
   document.querySelector("#question-title").innerHTML =
-    "All for and while loops can be expressed as a recursive function with an if statement?";
-  responseContentOne.textContent = "True";
-  responseContentTwo.textContent = "False";
-  responseContentThree.textContent = "Only for loops";
+    "All 'for' and 'while' loops can be expressed as a recursive function with an if statement?";
 
-  document.querySelector("#a").addEventListener("click", function () {
-    rightAnswer();
-    questionFive();
-  });
-  document.querySelector("#c").addEventListener("click", function () {
-    wrongAnswer();
-    questionFive();
-  });
-  document.querySelector("#b").addEventListener("click", function () {
-    wrongAnswer();
-    questionFive();
-  });
+  removeButtons();
+  eventsAndButtons("True", "False", "Only 'for' loops");
+
+  document.querySelector(".a").addEventListener("click", questionFive);
+  document.querySelector(".b").addEventListener("click", questionFive);
+  document.querySelector(".c").addEventListener("click", questionFive);
+  document.querySelector(".a").addEventListener("click", rightAnswer);
+  document.querySelector(".b").addEventListener("click", wrongAnswer);
+  document.querySelector(".c").addEventListener("click", wrongAnswer);
 };
 
 var questionFive = function () {
   document.querySelector("#question-title").innerHTML =
-    "What will be the outout of: console.log(5 + '6' + '4')?";
-  responseContentOne.textContent = "114";
-  responseContentTwo.textContent = "15";
-  responseContentThree.textContent = "564";
+    "What will be the output of: console.log(5 + '6' + '4')?";
 
-  document.querySelector("#c").addEventListener("click", function () {
-    rightAnswer();
-    endQuiz();
-  });
-  document.querySelector("#a").addEventListener("click", function () {
-    wrongAnswer();
-    endQuiz();
-  });
-  document.querySelector("#b").addEventListener("click", function () {
-    wrongAnswer();
-    endQuiz();
-  });
+  removeButtons();
+  eventsAndButtons("114", "15", "564");
+
+  document.querySelector(".a").addEventListener("click", endQuiz);
+  document.querySelector(".b").addEventListener("click", endQuiz);
+  document.querySelector(".c").addEventListener("click", endQuiz);
+  document.querySelector(".a").addEventListener("click", wrongAnswer);
+  document.querySelector(".b").addEventListener("click", wrongAnswer);
+  document.querySelector(".c").addEventListener("click", rightAnswer);
 };
 
 function countdown() {
@@ -157,6 +131,7 @@ function countdown() {
 
 function endQuiz() {
   let highScore = timeLeft;
+  removeButtons();
   if (highScore >= 0) {
     document.querySelector("#question-title").innerHTML =
       "You reached a score of " + highScore;
@@ -164,8 +139,4 @@ function endQuiz() {
     document.querySelector("#question-title").innerHTML =
       "Bro... You kinda suck";
   }
-}
-
-function quiz() {
-  countdown();
 }
